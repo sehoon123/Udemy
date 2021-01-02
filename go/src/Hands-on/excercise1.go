f4cf40cf2f0e4f67b4747efd6576b1f857a01bb7
sehoon123 <sehoon0519@naver.com>
2021-01-02 07:44:06 +0000

    ninja3 5

--- go/src/Hands-on/excercise1.go
+++ go/src/Hands-on/excercise1.go
@@ -1,13 +1,9 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	for i := 65; i <= 90; i++ {
-		for j := 1; j <= 3; j++ {
-			fmt.Printf("%v\t%#U\n", i, i)
-		}
+	for i := 0; i <= 100; i++ {
+		fmt.Println(i % 4)
 	}
 }
