78310f22d52740701a9bfc925761f1bc0d18f168
sehoon123 <sehoon0519@naver.com>
2021-01-03 13:40:45 +0000

    ninja4 8

--- go/src/Hands-on/excercise1.go
+++ go/src/Hands-on/excercise1.go
@@ -3,10 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	x := "James Bond"
-	if x == "James Bond" {
-		fmt.Println("Yes that's me")
-	} else {
-		fmt.Println("NO")
+	m := map[string][]string{
+		"bond_james":      []string{"Shaken", "not stirred", "Martinis", "Women"},
+		"moenypenny_miss": []string{"James Bond", "Literature", "Computer Science"},
 	}
+
+	for k, v := range m {
+		fmt.Printf("Key : %v\t, Value : %v\n", k, v)
+		fmt.Println("\t What I like : ")
+		for _, like := range v {
+			fmt.Printf("\t\t %v\n", like)
+		}
+	}
+
 }
