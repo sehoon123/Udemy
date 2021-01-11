f2ec45c4a5aac6d3d03cebf91e162af55e48c5a9
sehoon123 <sehoon0519@naver.com>
2021-01-11 15:01:25 +0000

    function in function

--- go/src/func/function.go
+++ go/src/func/function.go
@@ -3,15 +3,44 @@ package main
 import "fmt"
 
 func main() {
-	foo()
-	bar("Sehoon")
+	ii := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	a1 := sum(ii...)
+	fmt.Println(a1)
+
+	s2 := even(sum, ii...)
+	fmt.Println(s2)
+
+	s3 := odd(sum, ii...)
+	fmt.Println(s3)
 }
 
-func foo() {
-	fmt.Println("Hello, from foo")
+func sum(xi ...int) int {
+	total := 0
+	for _, v := range xi {
+		total += v
+	}
+
+	return total
 }
 
-// everything is Go is PASS BY VALUE
-func bar(s string) {
-	fmt.Println("Hello", s)
+func even(f func(xi ...int) int, vi ...int) int {
+	var yi []int
+	for _, v := range vi {
+		if v%2 == 0 {
+			yi = append(yi, v)
+		}
+	}
+
+	return f(yi...)
+}
+
+func odd(f func(xi ...int) int, vi ...int) int {
+	var yi []int
+	for _, v := range vi {
+		if v%2 == 1 {
+			yi = append(yi, v)
+		}
+	}
+
+	return f(yi...)
 }
