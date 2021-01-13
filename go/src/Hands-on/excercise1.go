c90577b20cb79e9938aadd40aaadbb43d5641969
sehoon123 <sehoon0519@naver.com>
2021-01-13 14:01:49 +0000

    func

--- go/src/Hands-on/excercise1.go
+++ go/src/Hands-on/excercise1.go
@@ -3,22 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	m := map[string][]string{
-		"bond_james":      []string{"Shaken", "not stirred", "Martinis", "Women"},
-		"moenypenny_miss": []string{"James Bond", "Literature", "Computer Science"},
+	a := foo()
+	fmt.Println(a())
+	fmt.Println(a())
+	fmt.Println(a())
+	fmt.Println(a())
+	fmt.Println(a())
+	fmt.Println(a())
+	fmt.Println(a())
+
+	b := foo()
+	fmt.Println(b())
+	fmt.Println(b())
+	fmt.Println(b())
+	fmt.Println(b())
+}
+
+func foo() func() int {
+	x := 0
+	return func() int {
+		x++
+		return x
 	}
-
-	m["sehoon"] = []string{"I", "Like", "You"}
-
-	for k, v := range m {
-		fmt.Println(k, v)
-		fmt.Println("\t What I like : ")
-		for _, like := range v {
-			fmt.Printf("\t\t %v\n", like)
-		}
-	}
-
-	delete(m, "sehoon")
-	fmt.Println(m)
-
 }
