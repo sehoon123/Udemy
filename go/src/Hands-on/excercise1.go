05ab837a46a08eb9a7b0604d45abdc5ffb39b22d
sehoon123 <sehoon0519@naver.com>
2021-01-13 14:38:42 +0000

    method set

--- go/src/Hands-on/excercise1.go
+++ go/src/Hands-on/excercise1.go
@@ -2,7 +2,25 @@ package main
 
 import "fmt"
 
+type person struct {
+	first string
+	last  string
+	age   int
+}
+
+func changeMe(p *person, n int) {
+	p.age = n
+}
+
 func main() {
-	x := 0
-	fmt.Println(&x)
+	p1 := person{
+		first: "Sehun",
+		last:  "Jung",
+		age:   23,
+	}
+	fmt.Println(p1)
+
+	changeMe(&p1, 10)
+
+	fmt.Println(p1)
 }
