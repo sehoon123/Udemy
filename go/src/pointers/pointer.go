b5accab26cf3e6b10a1ef00e20383b7908bfa0e8
sehoon123 <sehoon0519@naver.com>
2021-01-13 14:27:31 +0000

    with pointer

--- go/src/pointers/pointer.go
+++ go/src/pointers/pointer.go
@@ -4,12 +4,17 @@ import "fmt"
 
 func main() {
 	x := 0
-	foo(x)
-	fmt.Println(x)
+	fmt.Println("x before :", &x)
+	fmt.Println("x before: ", x)
+	foo(&x)
+	fmt.Println("x after :", &x)
+	fmt.Println("x after: ", x)
 }
 
-func foo(y int) {
-	fmt.Println(y)
-	y = 42
-	fmt.Println(y)
+func foo(y *int) {
+	fmt.Println("y before ", y)
+	fmt.Println("y before ", *y)
+	*y = 8
+	fmt.Println("y after ", y)
+	fmt.Println("y after ", *y)
 }
