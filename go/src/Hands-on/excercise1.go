9e66bd2844aa9c74e7d50b9eb24ceacb59eef628
sehoon123 <sehoon0519@naver.com>
2021-01-13 14:33:51 +0000

    ninja 7-1

--- go/src/Hands-on/excercise1.go
+++ go/src/Hands-on/excercise1.go
@@ -3,26 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	a := foo()
-	fmt.Println(a())
-	fmt.Println(a())
-	fmt.Println(a())
-	fmt.Println(a())
-	fmt.Println(a())
-	fmt.Println(a())
-	fmt.Println(a())
-
-	b := foo()
-	fmt.Println(b())
-	fmt.Println(b())
-	fmt.Println(b())
-	fmt.Println(b())
-}
-
-func foo() func() int {
 	x := 0
-	return func() int {
-		x++
-		return x
-	}
+	fmt.Println(&x)
 }
