265b3a36ce4c59c9d7c07f5bac5b39364fdd7364
sehoon123 <sehoon0519@naver.com>
2021-01-16 14:12:05 +0000

    ninja8-2

--- go/src/Hands-on/excercise1.go
+++ go/src/Hands-on/excercise1.go
@@ -5,34 +5,23 @@ import (
 	"fmt"
 )
 
-type user struct {
-	First string
-	Age   int
+type person struct {
+	First   string
+	Last    string
+	Age     int
+	Sayings []string
 }
 
 func main() {
-	u1 := user{
-		First: "James",
-		Age:   32,
-	}
+	s := `[{"First":"James","Last":"Bond","Age":32,"Sayings":["Shaken, not stirred","Youth is no guarantee of innovation","In his majesty's royal service"]},{"First":"Miss","Last":"Moneypenny","Age":27,"Sayings":["James, it is soo good to see you","Would you like me to take care of that for you, James?","I would really prefer to be a secret agent myself."]},{"First":"M","Last":"Hmmmm","Age":54,"Sayings":["Oh, James. You didn't.","Dear God, what has James done now?","Can someone please tell me where James Bond is?"]}]`
 
-	u2 := user{
-		First: "Moneypenny",
-		Age:   27,
-	}
+	var people []person
 
-	u3 := user{
-		First: "M",
-		Age:   54,
-	}
-
-	users := []user{u1, u2, u3}
-
-	fmt.Println(users)
-
-	bs, err := json.Marshal(users)
+	err := json.Unmarshal([]byte(s), &people)
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(string(bs))
+
+	fmt.Println(people)
+
 }
