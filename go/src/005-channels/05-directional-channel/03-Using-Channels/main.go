bf1d8349d80a9c38030cbd491bc7de019bbe2f9f
sehoon123 <sehoon0519@naver.com>
2021-01-23 13:07:22 +0000

    range prac

---
+++ go/src/005-channels/05-directional-channel/03-Using-Channels/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	c := make(chan int)
+
+	//send
+	go func() {
+
+		for i := 0; i < 5; i++ {
+			c <- i
+		}
+		// close(c)
+	}()
+
+	for v := range c {
+		fmt.Println(v)
+	}
+
+	fmt.Println("about to exit")
+}
