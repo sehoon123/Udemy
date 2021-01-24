bb48337a297cc03decab0431a420d7f6aad4aecf
sehoon123 <sehoon0519@naver.com>
2021-01-24 13:28:39 +0000

    fanIn prac

---
+++ go/src/005-channels/05-directional-channel/05-fanin/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	c := fanIn(boring("Joe"), boring("Ann"))
+	for i := 0; i < 10; i++ {
+		fmt.Println(<-c)
+	}
+	fmt.Println("You're both boring; I'm leaving.")
+}
+
+func boring(msg string) <-chan string {
+	c := make(chan string)
+	go func() {
+		for i := 0; ; i++ {
+			c <- fmt.Sprintf("%s %d", msg, i)
+			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
+		}
+	}()
+	return c
+}
+
+func fanIn(input1, input2 <-chan string) <-chan string {
+	c := make(chan string)
+	go func() {
+		for {
+			msg := <-input1
+			c <- msg
+		}
+	}()
+	go func() {
+		for {
+			msg := <-input2
+			c <- msg
+		}
+	}()
+	return c
+}
