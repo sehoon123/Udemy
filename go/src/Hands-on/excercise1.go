91551d3e8ac2611e8ab12ef1d20f07ac0bef44c8
sehoon123 <sehoon0519@naver.com>
2021-01-25 13:01:56 +0000

    ninja 10 -2

--- go/src/Hands-on/excercise1.go
+++ go/src/Hands-on/excercise1.go
@@ -5,7 +5,13 @@ import (
 )
 
 func main() {
-	c := make(chan int, 1)
-	c <- 42
-	fmt.Println(<-c)
+	cs := make(chan int)
+
+	go func() {
+		cs <- 42
+	}()
+	fmt.Println(<-cs)
+
+	fmt.Printf("------\n")
+	fmt.Printf("cs\t%T\n", cs)
 }
