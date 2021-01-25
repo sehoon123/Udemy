09453bc35029e5864ef3f4b446d3b8da57a3853f
sehoon123 <sehoon0519@naver.com>
2021-01-25 13:37:10 +0000

    ninja 10-5

--- go/src/Hands-on/excercise1.go
+++ go/src/Hands-on/excercise1.go
@@ -5,40 +5,17 @@ import (
 )
 
 func main() {
-	q := make(chan int)
-	c := gen(q)
-
-	receive(c, q)
-
-	fmt.Println("about to exit")
-}
-
-func receive(c, q <-chan int) {
-	for {
-
-		select {
-		case <-c:
-			fmt.Println("receive from c", <-c)
-		case <-q:
-			fmt.Println("receive from q", <-q)
-			return
-		}
-	}
-
-}
-
-func gen(q chan<- int) <-chan int {
 	c := make(chan int)
 
 	go func() {
-
-		for i := 0; i < 100; i++ {
-			c <- i
-		}
-		q <- 1
-		close(c)
-		close(q)
+		c <- 42
 	}()
 
-	return c
+	v, ok := <-c
+	fmt.Println(v, ok)
+
+	close(c)
+
+	v, ok = <-c
+	fmt.Println(v, ok)
 }
