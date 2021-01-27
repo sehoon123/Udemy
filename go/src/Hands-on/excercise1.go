3dad1ec46431db3f21ce6a359cc6791bb7d453e6
sehoon123 <sehoon0519@naver.com>
2021-01-27 13:47:26 +0000

    ninja 11-4

--- go/src/Hands-on/excercise1.go
+++ go/src/Hands-on/excercise1.go
@@ -1,22 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
-type customErr struct {
-	info string
+type sqrtError struct {
+	lat  string
+	long string
+	err  error
 }
 
-func (ce customErr) Error() string {
-	return fmt.Sprintf("\nhere is the error : %v", ce.info)
+//customError
+func (se sqrtError) Error() string {
+	return fmt.Sprintf("math error: %v %v %v", se.lat, se.long, se.err)
 }
 
 func main() {
-	c1 := customErr{
-		info: " need more coffee",
+	_, err := sqrt(-10.23)
+	if err != nil {
+		log.Println(err)
 	}
-	foo(c1)
 }
 
-func foo(e error) {
-	fmt.Println("foo ran - ", e)
+func sqrt(f float64) (float64, error) {
+	if f < 0 {
+		// write your error code here
+		e := fmt.Errorf("more coffee needed - value was %v\t", f)
+		return 0, sqrtError{"112 N", "50.5 W", e}
+	}
+	return 42, nil
 }
