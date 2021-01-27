9b314419dbcb5ef4d0b890e74c66efe7098aa83d
sehoon123 <sehoon0519@naver.com>
2021-01-27 13:01:00 +0000

    ninja 11-2

--- go/src/Hands-on/excercise1.go
+++ go/src/Hands-on/excercise1.go
@@ -18,10 +18,15 @@ func main() {
 		Sayings: []string{"Shaken, not stirred", "Any last wishes?", "Never say never"},
 	}
 
-	bs, err := json.Marshal(p1)
-	if err != nil {
-		fmt.Println(err)
-	}
+	bs, err := toJSON(p1)
+	fmt.Errorf("%T\n", err)
+
 	fmt.Println(string(bs))
 
+}
+
+// toJSON needs to return an error also
+func toJSON(a interface{}) ([]byte, error) {
+	bs, err := json.Marshal(a)
+	return bs, err
 }
