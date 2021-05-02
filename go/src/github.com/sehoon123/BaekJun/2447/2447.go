c361ba1f9c7da29b4a87d73b0f4b524d02bacbec
sehoon123 <sehoon0519@naver.com>
2021-05-02 13:34:52 +0000

    2021-05-02

--- go/src/github.com/sehoon123/BaekJun/2447/2447.go
+++ go/src/github.com/sehoon123/BaekJun/2447/2447.go
@@ -35,14 +35,40 @@ func main() {
 	// star(n)
 }
 
+// func clear(stars [][]int, n int) {
+// 	if n == 1 {
+// 		return
+// 	}
+// 	for i := 0; i < n/3; i++ {
+// 		for j := 0; j < n/3; j++ {
+// 			stars[n/3+i][n/3+j] = 1
+// 		}
+// 	}
+// 	clear(stars, n/3)
+// }
+
 func clear(stars [][]int, n int) {
-	if n == 1 {
-		return
-	}
-	for i := 0; i < n/3; i++ {
-		for j := 0; j < n/3; j++ {
-			stars[n/3+i][n/3+j] = 1
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			if i%3 == 1 && j%3 == 1 {
+				stars[i][j] = 1
+			} else if (i/3)%3 == 1 && (j/3)%3 == 1 {
+				stars[i][j] = 1
+			} else if (i/9)%3 == 1 && (j/9)%3 == 1 {
+				stars[i][j] = 1
+			} else if (i/27)%3 == 1 && (j/27)%3 == 1 {
+				stars[i][j] = 1
+			} else if (i/81)%3 == 1 && (j/81)%3 == 1 {
+				stars[i][j] = 1
+			} else if (i/243)%3 == 1 && (j/243)%3 == 1 {
+				stars[i][j] = 1
+			} else if (i/729)%3 == 1 && (j/729)%3 == 1 {
+				stars[i][j] = 1
+			} else if (i/2187)%3 == 1 && (j/2187)%3 == 1 {
+				stars[i][j] = 1
+			} else if (i/6561)%3 == 1 && (j/6561)%3 == 1 {
+				stars[i][j] = 1
+			}
 		}
 	}
-	clear(stars, n/3)
 }
