254ece9104ddf14261437557ca59fed5481f384d
sehoon123 <60769447+sehoon123@users.noreply.github.com>
2021-06-16 09:58:59 +0900

    Update 11729.go

--- go/src/github.com/sehoon123/BaekJun/11729/11729.go
+++ go/src/github.com/sehoon123/BaekJun/11729/11729.go
@@ -8,16 +8,29 @@ import (
 )
 
 func main() {
-	r, w := bufio.NewReader(os.Stdin), bufio.NewWriter(os.Stdout)
+	r, w := bufio.NewReader(os.Stdin),bufio.NewWriter(os.Stdout)
 	defer w.Flush()
-
 	var n int
+	fmt.Fscanf(r,"%d\n", &n)
+	fmt.Fprintf(w,"%.f\n", math.Pow(2,float64(n))-1)
+	hanoi(w, n,1,2,3)
 
-	fmt.Fscanf(r, "%d\n", &n)
-	fmt.Fprintf(w, "%.f\n", math.Pow(2, float64(n))-1)
 }
 
-func hanoi(k int) {
-	if k%2 == 0 {
+
+
+func hanoi(w *bufio.Writer,N, start, mid, to int) {
+	if N == 1 {
+		fmt.Fprintf(w,"%d %d\n", start, to)
+		return
 	}
+
+	hanoi(w,N-1, start, to, mid)
+
+	fmt.Fprintf(w,"%d %d\n", start, to)
+
+	hanoi(w,N-1, mid, start, to)
+
+
+
 }
